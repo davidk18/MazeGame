97d6a97fc4ba41ccea456bf832dfe40807c56872
David Kearney <15169235@studentmail.ul.ie>
2019-04-21 01:13:52 +0100

    Main refactored, save/load debugged, Interpreter implemented

--- src/com/group4/Objects/InputReceiver.java
+++ src/com/group4/Objects/InputReceiver.java
@@ -1,7 +1,7 @@
 package com.group4.Objects;
 import java.util.Scanner;
 public class InputReceiver {
-
+// fu git
     public static String getInput(){
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
